9e5a85c643d3194a14d079219a9f4be8007b8e04
unknown <dgmouratorres@gmail.com>
2022-05-12 11:27:02 -0300

    Crie funcionalidade do site

---
+++ index.js
@@ -0,0 +1,15 @@
+let num1 = Math.floor(Math.random() * 6 + 1);
+let num2 = Math.floor(Math.random() * 6 + 1);
+
+document.querySelector(".img1").src = `imagens/dice${num1}.png`;
+document.querySelector(".img2").src = `imagens/dice${num2}.png`;
+
+let titulo = document.querySelector("h1");
+
+if (num1 > num2) {
+    titulo.textContent = "Jogador 1 Venceu!";
+} else if (num1 < num2) {
+    titulo.textContent = "Jogador 2 Venceu!";
+} else {
+    titulo.textContent = "Empate!";
+}
