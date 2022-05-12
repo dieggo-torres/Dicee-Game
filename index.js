19d4d4fdbb4c686639161716ecc57695f41e035d
Diego Moura Torres <95168133+dieggo-torres@users.noreply.github.com>
2022-05-12 12:01:08 -0300

    Atualize index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let num1 = Math.floor(Math.random() * 6 + 1);
-let num2 = Math.floor(Math.random() * 6 + 1);
+let num1 = Math.floor(Math.random() * 6) + 1;
+let num2 = Math.floor(Math.random() * 6) + 1;
 
 document.querySelector(".img1").src = `imagens/dice${num1}.png`;
 document.querySelector(".img2").src = `imagens/dice${num2}.png`;
@@ -7,9 +7,9 @@ document.querySelector(".img2").src = `imagens/dice${num2}.png`;
 let titulo = document.querySelector("h1");
 
 if (num1 > num2) {
-    titulo.textContent = "Jogador 1 Venceu!";
+    titulo.textContent = "🚩 Jogador 1 Venceu!";
 } else if (num1 < num2) {
-    titulo.textContent = "Jogador 2 Venceu!";
+    titulo.textContent = "Jogador 2 Venceu! 🚩";
 } else {
     titulo.textContent = "Empate!";
 }
